1e4760e317d9f0dd82fee75d7f2f60697e044276
PiotrPress <admin@piotr.press>
2023-03-08 13:18:19 +0100

    PP-14248

--- plugin.php
+++ plugin.php
@@ -6,17 +6,18 @@
 
 defined( 'ABSPATH' ) or exit;
 
-add_action( 'wp_ajax_nopriv_piwik_pro_account_exists', function() {
-    ! empty( $_POST[ 'account' ] ) or wp_die( 0, 400 );
+foreach( [ 'wp_ajax_nopriv', 'wp_ajax' ] as $prefix )
+    add_action( $prefix . '_piwik_pro_account_exists', function() {
+        ! empty( $_POST[ 'account' ] ) or wp_die( 0, 400 );
 
-    $url = esc_url(
-        'https://' .
-        trim( preg_replace('(^https?://)', '', $_POST[ 'account' ] ), '/' ) .
-        '.piwik.pro'
-    );
+        $url = esc_url(
+            'https://' .
+            trim( preg_replace('(^https?://)', '', $_POST[ 'account' ] ), '/' ) .
+            '.piwik.pro'
+        );
 
-    $response = wp_remote_get( $url );
-    $code = (int)( 200 === wp_remote_retrieve_response_code( $response ) );
+        $response = wp_remote_get( $url );
+        $code = (int)( 200 === wp_remote_retrieve_response_code( $response ) );
 
-    wp_die( $code, $code ? 200 : 404 );
-} );
+        wp_die( $code, $code ? 200 : 404 );
+    } );
